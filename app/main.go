b3de2614de7207c9640c7719fca27cb0de6f6c0b
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-03-01 20:42:19 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -24,37 +25,65 @@ func BS[T any](arr []T, target T, l int, r int, less func(a, b T) bool) (int, bo
 	return mid, true
 }
 
+func Executable(arg string) (bool, string) {
+	pathEnv := os.Getenv("PATH")
+	dirs := strings.Split(pathEnv, ":")
+	for _, dir := range dirs {
+		full_path := filepath.Join(dir, arg)
+		info, err := os.Stat(full_path)
+		if err != nil {
+			continue
+		}
+		if !info.IsDir() && info.Mode()&0111 != 0 {
+			return true, full_path
+		}
+	}
+	return false, ""
+}
+
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	comm := []string{"echo", "exit", "type"}
 	for {
 		fmt.Print("$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		commandLn, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err == nil {
-			if strings.EqualFold(strings.ToLower(strings.TrimSpace(command[:len(command)-1])), "exit") {
+			if strings.ToLower(strings.TrimSpace(commandLn[:len(commandLn)-1])) == "exit" {
 				break
 			}
-			if strings.ToLower(strings.TrimSpace(strings.Split(command[:len(command)-1], " ")[0])) == "echo" {
-				for i := 1; i < len(strings.Split(command[:len(command)-1], " ")); i++ {
-					fmt.Print(strings.Split(command[:len(command)-1], " ")[i] + " ")
-				}
-				fmt.Println()
-				continue
-			}
-			if strings.ToLower(strings.TrimSpace(strings.Split(command[:len(command)-1], " ")[0])) == "type" {
-				comp := func(a, b string) bool {
-					return a < b
-				}
-				if _, ch := BS(comm, strings.ToLower(strings.TrimSpace(strings.Split(command[:len(command)-1], " ")[1])), 0, 2, comp); ch {
-					fmt.Printf("%s is a shell builtin", strings.ToLower(strings.TrimSpace(strings.Split(command[:len(command)-1], " ")[1])))
-				} else {
-					fmt.Printf("%s: not found", strings.ToLower(strings.TrimSpace(strings.Split(command[:len(command)-1], " ")[1])))
+			command := strings.ToLower(strings.TrimSpace(strings.Split(commandLn[:len(commandLn)-1], " ")[0]))
+			if command == "echo" {
+				for i := 1; i < len(strings.Split(commandLn[:len(commandLn)-1], " ")); i++ {
+					fmt.Print(strings.Split(commandLn[:len(commandLn)-1], " ")[i] + " ")
 				}
 				fmt.Println()
 				continue
 			}
 
-			fmt.Printf("%s: command not found", command[:len(command)-1])
+			// Type command
+			if command == "type" {
+				comp := func(a, b string) bool {
+					return a < b
+				}
+				// Built in
+				arg := strings.ToLower(strings.TrimSpace(strings.Split(commandLn[:len(commandLn)-1], " ")[1]))
+				if _, ch := BS(comm, arg, 0, 2, comp); ch {
+					fmt.Printf("%s is a shell builtin", arg)
+				} else {
+					// Search for executable files using PATH.
+					ch, path := Executable(arg)
+					if ch {
+						fmt.Printf("%s is %s", arg, path)
+					} else {
+						fmt.Printf("%s: not found", strings.ToLower(strings.TrimSpace(strings.Split(commandLn[:len(commandLn)-1], " ")[1])))
+					}
+				}
+				fmt.Println()
+				continue
+			}
+
+			// Not found
+			fmt.Printf("%s: command not found", command)
 			fmt.Println()
 		} else {
 			fmt.Print(err)
