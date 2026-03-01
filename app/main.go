0fa31c95ab51dce356a1a1b6addbb0fb161cec49
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-03-01 15:12:16 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,12 +11,14 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err == nil {
-		fmt.Printf("%s: command not found", command[:len(command)-1])
-		fmt.Println()
-	} else {
-		fmt.Print(err)
+	for {
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err == nil {
+			fmt.Printf("%s: command not found", command[:len(command)-1])
+			fmt.Println()
+		} else {
+			fmt.Print(err)
+		}
 	}
 }
