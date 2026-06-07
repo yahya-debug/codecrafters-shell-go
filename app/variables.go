68a6d347614674854b5a14557239c0a6228ed15a
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-06-07 13:03:19 +0300

    codecrafters submit [skip ci]

--- app/variables.go
+++ app/variables.go
@@ -15,7 +15,7 @@ func removeVar(key string) {
 func printVar(key string) string {
 	val, ok := shellVariables[key]
 	if ok {
-		return fmt.Sprintf("declare -- %s=\"%s\"", key, val)
+		return fmt.Sprintf("declare -- %s=\"%s\"\n", key, val)
 	} else {
 		return fmt.Sprintf("declare: %s: not found\n", key)
 	}
